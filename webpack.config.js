c0dcf2f314464e2109764cca2fcd70689ac7d5ab
Luka Glusica <luka.glusica89@gmail.com>
2017-06-04 22:25:09 +0200

    Add source files from previous version of the library and modify webpack config accordingly

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,23 @@ const webpack = require('webpack');
 module.exports = {
    context: path.resolve(__dirname, './src'),
    entry: {
-      app: ['./test.js'],
+      app: [
+         "./Configuration.js",
+         "./Date.js",
+         "./ObservedProperty.js",
+         "./ProcedureBase.js",
+         "./Service.js",
+         "./Database.js",
+         "./EventHandling.js",
+         "./Offering.js",
+         "./Procedure.js",
+         "./UnitOfMeasure.js",
+         "./DataQuality.js",
+         "./IstSOS.js",
+         "./Output.js",
+         "./Server.js",
+         "./VirtualProcedure.js"
+      ],
    },
    output: {
       path: path.resolve(__dirname, './dist'),
@@ -22,11 +38,15 @@ module.exports = {
             }],
          },
          {
-            test: /\/src\/.*\.js$/,
-            loaders: [
-               'closure-loader'
-            ],
-            exclude: [/node_modules/, /test/]
+             test: /src\/\.*\.js/,
+             loader: path.resolve(__dirname, '../..'),
+             options: {
+                 paths: [
+                     path.resolve(__dirname, 'src/'),
+                 ],
+                 es6mode: true,
+             },
+             exclude: [/test/, /node_modules/],
          }
       ],
    }
