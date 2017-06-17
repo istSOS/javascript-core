ba1bd9318e13f6b8858bb2a0c63e556bf01daeba
Luka Glusica <luka.glusica89@gmail.com>
2017-06-17 13:52:48 +0200

    Rewrite to ES6 and refactor istsos.Server class

--- src/Server.js
+++ src/Server.js
@@ -1,7 +1,7 @@
-goog.require("goog.events");
-goog.require("goog.events.Event");
-goog.require("goog.events.EventTarget");
+goog.provide("istsos.Server");
+goog.require("istsos.Configuration");
 goog.require("goog.net.XhrIo");
+goog.require("istsos.events.EventType")
 
 /** istsos.Server class */
 /**
@@ -12,128 +12,142 @@ goog.require("goog.net.XhrIo");
  * @param {JSON} opt_loginConfig
  * @constructor
  */
-istsos.Server = function (serverName, url, defaultDb, opt_config, opt_loginConfig) {
-    this.serverName = serverName;
-    this.url = (url.charAt(url.length - 1) === "/") ? url : url + "/";
-    this.defaultDb = defaultDb;
-    this.config = opt_config || new istsos.Configuration(null, this);
-    this.loginConfig = opt_loginConfig || {};
-    this.services = [];
-    this.login();
-};
+istsos.Server = class {
+   constructor(options) {
+      this.name = options.name;
+      this.url = (otpions.url.charAt(otpions.url.length - 1) === "/") ? otpions.url : otpions.url + "/";
+      this.defaultDb = otpions.defaultDb;
+      this.config = otpions.opt_config || new istsos.Configuration({
+         serviceName: null,
+         server: this
+      });
+      this.loginConfig = otpions.opt_loginConfig || {};
+      this.services = [];
+      this.login();
+   }
 
-istsos.Server.prototype = {
-    /**
-     * @param {String} url
-     * @param {istsos.events.EventType} eventType
-     * @param {String} method
-     * @param {JSON} opt_data
-     * @param {function} opt_callback
-     */
-    executeRequest: function (url, eventType, method, opt_data, opt_callback) {
-        goog.net.XhrIo.send(url, function (e) {
-            var obj = e.target.getResponseJson();
-            console.log(obj);
-            istsos.fire(eventType, e.target);
-        }, method, opt_data);
-    },
+   /**
+    * @param {String} url
+    * @param {istsos.events.EventType} eventType
+    * @param {String} method
+    * @param {JSON} opt_data
+    */
+   executeRequest(url, eventType, method, opt_data) {
+      goog.net.XhrIo.send(url, function(e) {
+         istsos.fire(eventType, e.target);
+      }, method, opt_data);
+   }
 
-    login: function() {
-        var authStr = this.loginConfig.user + ":" + this.loginConfig.password + "@";
-        var url = this.url.match(/http:/gi) ?
-        [this.url.slice(0,7), authStr, this.url.slice(7), "wa/istsos/operations/status"].join("") : "http://" + authStr + this.url + "wa/istsos/operations/status";
-        this.executeRequest(url, istsos.events.EventType.LOGIN, "GET");
-    },
-    /**
-     * @fires istsos.Server#istsos.events.EventType: SERVICE
-     * @param {istsos.Service} service
-     */
-    getService: function (service) {
-        var url = this.url + "wa/istsos/services/" + service.getServiceJSON()["service"];
-        this.executeRequest(url, istsos.events.EventType.SERVICE, "GET");
-    },
-    /**
-     * @param {istsos.Service} service
-     */
-    addService: function (service) {
-        this.services.push(service);
-    },
-    /**
-     * @fires istsos.Service#istsos.events.EventType: NEW_SERVICE
-     * @param {istsos.Service} service
-     */
-    registerService: function (service) {
-        var url = this.getUrl() + "wa/istsos/services";
-        this.executeRequest(url, istsos.events.EventType.NEW_SERVICE, "POST", JSON.stringify(service.getServiceJSON()));
-    },
-    /**
-     * @fires istsos.Service#istsos.events.EventType: DELETE_SERVICE
-     * @param {istsos.Service} service
-     */
-    deleteService: function (service) {
-        for (var i = 0; i < this.services.length; i++) {
-            if (this.services[i].getServiceJSON()["service"] === service.getServiceJSON()["service"]) {
-                this.services.splice(i, 1);
-            }
-        }
-        var url = this.url + "wa/istsos/services/" + service.serviceName;
-        this.executeRequest(url, istsos.events.EventType.DELETE_SERVICE, "DELETE", JSON.stringify({"name": service.serviceName}));
-    },
-    /**
-     * @fires istsos.Service#istsos.events.EventType: STATUS
-     */
-    getStatus: function () {
-        var url = this.url + "wa/istsos/operations/status";
-        this.executeRequest(url, istsos.events.EventType.STATUS, "GET");
-    },
-    /**
-     * @fires istsos.Service#istsos.events.EventType: ABOUT
-     */
-    getAboutInfo: function () {
-        var url = this.url + "wa/istsos/operations/about";
-        this.executeRequest(url, istsos.events.EventType.ABOUT, "GET");
-    },
-    /**
-     * @fires istsos.Configuration#istsos.events.EventType: CONFIGURATION
-     */
-    getConfig: function () {
-        this.config.getConf();
-    },
-    /**
-     * @returns {istsos.Configuration}
-     */
-    getConfigProperty: function () {
-        return this.config;
-    },
-    /**
-     * @returns {Array<istsos.Service>}
-     */
-    getServicesProperty: function () {
-        return this.services
-    },
-    /**
-     * @fires istsos.Service#istsos.events.EventType: SERVICES
-     */
-    getServices: function () {
-        var url = this.url + "wa/istsos/services";
-        this.executeRequest(url, istsos.events.EventType.SERVICES, "GET");
-    },
-    /**
-     * @fires istsos.Database#istsos.events.EventType: DATABASE
-     */
-    getDefaultDb: function () {
-        this.defaultDb.getDb("default", this);
-    },
-    /**
-     * @returns {istsos.Database}
-     */
-    getDefaultDbProperty: function () {
-        return this.defaultDb;
-    },
-    /**
-     * @returns {String}
-     */
-    getUrl: function () {
-        return this.url;
-    }
-};
+   login() {
+      var authStr = `${this.loginConfig.user}:${this.loginConfig.password}@`;
+      var url = this.url.match(/http:/gi) ? [this.url.slice(0, 7), authStr, this.url.slice(7), "wa/istsos/operations/status"].join("") : `http://${authStr}${this.url}wa/istsos/operations/status`;
+      this.executeRequest(url, istsos.events.EventType.LOGIN, "GET");
+   }
+
+   /**
+    * @fires istsos.Server#istsos.events.EventType: SERVICE
+    * @param {istsos.Service} service
+    */
+   getService(service) {
+      var url = `${this.url}wa/istsos/services/${service.name}`;
+      this.executeRequest(url, istsos.events.EventType.SERVICE, "GET");
+   }
+
+   /**
+    * @param {istsos.Service} service
+    */
+   addService(service) {
+      this.services.push(service);
+   }
+
+   /**
+    * @fires istsos.Service#istsos.events.EventType: NEW_SERVICE
+    * @param {istsos.Service} service
+    */
+   registerService(service) {
+      var url = `${this.getUrl()}wa/istsos/services`;
+      this.executeRequest(url, istsos.events.EventType.NEW_SERVICE, "POST", JSON.stringify(service.getServiceJSON()));
+   }
+
+   /**
+    * @fires istsos.Service#istsos.events.EventType: DELETE_SERVICE
+    * @param {istsos.Service} service
+    */
+   deleteService(service) {
+      for (var i = 0; i < this.services.length; i++) {
+         if (this.services[i].getServiceJSON()["service"] === service.getServiceJSON()["service"]) {
+            this.services.splice(i, 1);
+         }
+      }
+      var url = `${this.url}wa/istsos/services/${service.name}`;
+      this.executeRequest(url, istsos.events.EventType.DELETE_SERVICE, "DELETE", JSON.stringify({
+         "name": service.name
+      }));
+   }
+
+   /**
+    * @fires istsos.Service#istsos.events.EventType: STATUS
+    */
+   getStatus() {
+      var url = `${this.url}wa/istsos/operations/status`;
+      this.executeRequest(url, istsos.events.EventType.STATUS, "GET");
+   }
+
+   /**
+    * @fires istsos.Service#istsos.events.EventType: ABOUT
+    */
+   getAboutInfo() {
+      var url = `${this.url}wa/istsos/operations/about`;
+      this.executeRequest(url, istsos.events.EventType.ABOUT, "GET");
+   }
+
+   /**
+    * @fires istsos.Configuration#istsos.events.EventType: CONFIGURATION
+    */
+   getConfig() {
+      this.config.getConf();
+   }
+
+   /**
+    * @returns {istsos.Configuration}
+    */
+   getConfigProperty() {
+      return this.config;
+   }
+
+   /**
+    * @returns {Array<istsos.Service>}
+    */
+   getServicesProperty() {
+      return this.services
+   }
+
+   /**
+    * @fires istsos.Service#istsos.events.EventType: SERVICES
+    */
+   getServices() {
+      var url = `${this.url}wa/istsos/services`;
+      this.executeRequest(url, istsos.events.EventType.SERVICES, "GET");
+   }
+
+   /**
+    * @fires istsos.Database#istsos.events.EventType: DATABASE
+    */
+   getDefaultDb() {
+      this.defaultDb.getDb("default", this);
+   }
+
+   /**
+    * @returns {istsos.Database}
+    */
+   getDefaultDbProperty() {
+      return this.defaultDb;
+   }
+
+   /**
+    * @returns {String}
+    */
+   getUrl() {
+      return this.url;
+   }
+}
