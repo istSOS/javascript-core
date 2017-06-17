8ad2bbcb796ea75e2b9e43eb0d9611a5ac7b30cb
Luka Glusica <luka.glusica89@gmail.com>
2017-06-17 16:06:58 +0200

    Rewrite to ES6 and refactor istsos.DataQuality class

--- src/DataQuality.js
+++ src/DataQuality.js
@@ -10,67 +10,62 @@ goog.require('goog.net.XhrIo');
  * @param {String} descrDQ
  * @constructor
  */
-istsos.DataQuality = function (service, codeDQ, nameDQ, descrDQ) {
-    this.code = codeDQ;
-    this.name = nameDQ;
-    this.description = descrDQ || "";
-    this.service = service;
-    service.addDataQuality(this);
-};
+istsos.DataQuality = class {
+   constructor(options) {
+      this.code = options.codeDQ;
+      this.name = options.nameDQ;
+      this.description = options.descrDQ || "";
+      this.service = options.service;
+      service.addDataQuality(this);
+   }
 
-istsos.DataQuality.prototype = {
-    /**
-     * @param {String} url
-     * @param {istsos.events.EventType} eventType
-     * @param {String} method
-     * @param {JSON} opt_data
-     * @param {function} opt_callback
-     */
-    executeRequest: function (url, eventType, method, opt_data, opt_callback) {
-        goog.net.XhrIo.send(url, function (e) {
-            var obj = e.target.getResponseJson();
-            console.log(obj);
-            istsos.fire(eventType, e.target);
-        }, method, opt_data);
-    },
-    /**
-     * @returns {JSON}
-     */
-    getDataQualityJSON: function () {
-        var dqJSON = {
-            "code": this.code.toString(),
-            "name": this.name,
-            "description": this.description
-        };
-        return dqJSON;
-    },
-    /**
-     * @fires istsos.DataQuality#istsos.events.EventType: UPDATE_DATAQUALITY
-     * @param {int} newCodeDQ
-     * @param {String} newNameDQ
-     * @param {String} newDescrDQ
-     */
-    updateDataQuality: function ( newCodeDQ, newNameDQ, newDescrDQ) {
-        var oldName = this.code;
-        this.code = newCodeDQ || this.code;
-        this.name = newNameDQ || this.name;
-        this.description = newDescrDQ || this.description;
-        var url = this.service.server.getUrl() + "wa/istsos/services/" + this.service.getServiceJSON()["service"] +
-            "/dataqualities/" + oldName;
-        this.executeRequest(url, istsos.events.EventType.UPDATE_DATAQUALITY, "PUT", JSON.stringify(this.getDataQualityJSON()));
-    },
-    /**
-     * @fires istsos.DataQuality#istsos.events.EventType: DELETE_DATAQUALITY
-     */
-    deleteDataQuality: function () {
-        var dataQualities = this.service.getDataQualitiesProperty();
-        for (var i = 0; i < dataQualities.length; i++) {
-            if (this.code === dataQualities[i]["code"]) {
-                dataQualities.splice(i, 1);
-            }
-        }
-        var url = this.service.server.getUrl() + "wa/istsos/services/" + this.service.getServiceJSON()["service"] +
-            "/dataqualities/" + this.getDataQualityJSON()["code"];
-        this.executeRequest(url, istsos.events.EventType.DELETE_DATAQUALITY, "DELETE", JSON.stringify(this.getDataQualityJSON()));
-    }
-};
+   executeRequest(url, eventType, method, opt_data) {
+      goog.net.XhrIo.send(url, function(e) {
+         istsos.fire(eventType, e.target);
+      }, method, opt_data);
+   }
+
+   /**
+    * @returns {JSON}
+    */
+   getDataQualityJSON() {
+      var dqJSON = {
+         "code": this.code.toString(),
+         "name": this.name,
+         "description": this.description
+      };
+      return dqJSON;
+   }
+
+   /**
+    * @fires istsos.DataQuality#istsos.events.EventType: UPDATE_DATAQUALITY
+    * @param {int} newCodeDQ
+    * @param {String} newNameDQ
+    * @param {String} newDescrDQ
+    */
+   updateDataQuality(options) {
+      const oldName = this.code;
+      this.code = options.newCodeDQ || this.code;
+      this.name = options.newNameDQ || this.name;
+      this.description = options.newDescrDQ || this.description;
+
+      var url = `${this.service.server.getUrl()}wa/istsos/services/${this.service.getServiceJSON()["service"]}/dataqualities/${oldName}`;
+      this.executeRequest(url, istsos.events.EventType.UPDATE_DATAQUALITY, "PUT", JSON.stringify(this.getDataQualityJSON()));
+   }
+
+   /**
+    * @fires istsos.DataQuality#istsos.events.EventType: DELETE_DATAQUALITY
+    */
+   deleteDataQuality() {
+      var dataQualities = this.service.getDataQualitiesProperty();
+      for (var i = 0; i < dataQualities.length; i++) {
+         if (this.code === dataQualities[i]["code"]) {
+            dataQualities.splice(i, 1);
+         }
+      }
+
+      var url = `${this.service.server.getUrl()}wa/istsos/services/${this.service.getServiceJSON()["service"]}/dataqualities/${this.getDataQualityJSON()["code"]}`;
+      this.executeRequest(url, istsos.events.EventType.DELETE_DATAQUALITY, "DELETE", JSON.stringify(this.getDataQualityJSON()));
+   }
+
+}
