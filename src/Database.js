0fa9589be98e5b192a7cb257f34c41c5b03e11c3
Luka Glusica <luka.glusica89@gmail.com>
2017-06-28 21:57:47 +0200

    Refine istsos.Database class

--- src/Database.js
+++ src/Database.js
@@ -2,9 +2,18 @@ import {HttpAPI } from 'HttpAPI';
 import {EventEmitter } from 'EventEmitter';
 
 /**
- * @class istsos.Database
+ * istsos.Database
+ * 
+ * @class
+ * @extends istsos.EventEmitter
  */
 export var Database = class Database extends EventEmitter {
+   /**
+    * constructor - instantiates istsos.Database
+    * 
+    * @param  {Object} options Set of key-value pairs
+    * @constructor
+    */
    constructor(options) {
       super();
       this.dbname = options.dbname;
@@ -14,26 +23,64 @@ export var Database = class Database extends EventEmitter {
       this.port = options.port;
    }
 
+   /**
+    * Fire event with data - event must match one of the supported event types from istsos.EventTypes
+    * 
+    * @param  {String} eventType Type of event from istsos.EventTypes
+    * @param  {Object|*} response  Data to be passed to a handler
+    */
    fireEvent(eventType, response) {
       super.fire(eventType, response)
    }
 
+   /**
+    * Add event listener
+    * 
+    * @param  {String}   event    Event must match one of the supported event types from istsos.EventTypes
+    * @param  {Function} callback Handler function
+    */
    on(event, callback) {
       super.on(event, callback);
    }
 
+   /**
+    * Add event listener, that will listen only once.
+    * 
+    * @param  {String}   event    Event must match one of the supported event types from istsos.EventTypes
+    * @param  {Function} callback Handler function
+    */
    once(event, callback) {
       super.once(event, callback);
    }
 
+   /**
+    * Remove event listener
+    * 
+    * @param  {String}   event    Event must match one of the supported event types from istsos.EventTypes
+    * @param  {Function} callback Handler function
+    */
    off(event, callback) {
       super.off(event, callback);
    }
 
+   /**
+    * Remove all event listeners
+    * 
+    * @param  {String}   event    Event must match one of the supported event types from istsos.EventTypes
+    * @param  {Function} callback Handler function
+    */
    unlistenAll() {
-      super.unlistenAll(event, callback);
+      super.unlistenAll();
    }
 
+   /**
+    * Get database information from the server
+    * 
+    * @param  {String} serviceName   Name of the service
+    * @param  {istsos.Server} server Instance of istsos.Server class
+    * @return {Promise} 
+    * @fires  istsos.Database#DATABASE            
+    */
    getDb(serviceName = 'default', server) {
       var url = `${server.getUrl()}wa/istsos/services/${serviceName}/configsections/connection`;
       
@@ -55,6 +102,15 @@ export var Database = class Database extends EventEmitter {
          });
    }
 
+   /**
+    * Set database connection parameters
+    * 
+    * @param {istsos.Server} server  istsos.Server instance
+    * @param {istsos.Service} service istsos.Service instance
+    * @param {Object} options Set of key-value pairs
+    * @return {Promise}
+    * @fires istsos.Database#UPDATE_DATABASE
+    */
    setDb(server, service, options) {
       this.dbname = options.dbname || this.dbname;
       this.host = options.host || this.host;
@@ -84,6 +140,13 @@ export var Database = class Database extends EventEmitter {
          });
    }
 
+   /**
+    * Validate database status
+    * 
+    * @param  {istsos.Server} server istsos.Server instance
+    * @return {Promise}
+    * @fires istsos.Database#VALIDATE_DB
+    */
    validateDb(server) {
       var url = `${server.getUrl()}wa/istsos/operations/validatedb`;
       
@@ -107,6 +170,11 @@ export var Database = class Database extends EventEmitter {
          });
    }
 
+   /**
+    * Get JSON configuration prepared for sending via HTTP request payload
+    * 
+    * @return {Object}
+    */
    getDbJSON() {
       return {
          "dbname": this.dbname,
